ff57464c6d35878f7ac766fd8b03ad97cf79c356
Usuario <Usuario@CPD03-M07>
2024-03-26 11:46:18 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMatri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("informe o valor da aniudade: ");
+            double anual = double.Parse(Console.ReadLine());
+            double mensalidade = anual / 12;
+            Console.WriteLine("informe o valor da matrícula: ");
+            double matri = double.Parse(Console.ReadLine());
+            Console.WriteLine("informe o valor do material: ");
+            double material = double.Parse(Console.ReadLine());
+
+            int cont = 1;
+            while (cont <= 12) 
+            {
+                if (cont == 1) {
+                Console.WriteLine("Mensalidade: " +  cont + "valor: ");
+                }
+            }
+        }
+    }
+}
